7b3cd6cc2bc58ecbef78b23d7d692aedbd052624
Sergey Karavay <siarheikaravai@rentalcars.com>
2018-08-24 10:02:44 +0100

    Fix setting name case

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -10,7 +10,7 @@ return [
         'name' => 'Blog Time to Read',
         'description' => 'Set default reading options',
         'main_settings_title' => 'Main Settings',
-        'default_reading_speed' => 'Default reading Speed',
+        'default_reading_speed' => 'Default reading speed',
         'default_reading_speed_comment' => 'Words per minute by default',
         'rounding_up_enabled' => 'Rounding up',
         'rounding_up_enabled_comment' => 'Round fractional values to the next highest integer value'
